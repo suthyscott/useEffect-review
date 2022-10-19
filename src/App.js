69cc8b7c5247b39a89bf5e476ef665bdfc3b72e5
suthyscott <sutherlandscott21@gmail.com>
2022-10-19 09:16:27 -0600

    useEffect review

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [chars, setChars] = useState([])
+
+  const getAllChars = () => {
+    axios.get('https://hp-api.herokuapp.com/api/characters')
+      .then(res => setChars(res.data))
+  }
+
+  useEffect(() => {
+    getAllChars()
+  }, [])
+
+  console.log(chars)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      app.js
     </div>
   );
 }
