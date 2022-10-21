d9bba512285995b21b8c099d5a5c0c0d606bed80
suthyscott <sutherlandscott21@gmail.com>
2022-10-21 09:23:24 -0600

    add to wishlist

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Counter from './Counter';
 
 function App() {
   const [chars, setChars] = useState([])
+  const [favorites, setFavorites] = useState([])
 
   const getAllChars = () => {
     axios.get('https://hp-api.herokuapp.com/api/characters')
@@ -16,13 +17,28 @@ function App() {
     getAllChars()
   }, [])
 
-  console.log(chars)
+  const addToFavs = char => {
+      setFavorites(prevState => [...prevState, char])
+      setChars(prevState => prevState.filter(elem => elem.name !== char.name))
+  }
+
+  // console.log(chars)
   return (
     <div className="App">
-      <Counter/>
-      {/* {chars.map((char, index) => {
-        return <CharDisplay char={char}/>
-      })} */}
+      {/* <Counter/> */}
+      <div>
+        <h1>All Chars</h1>
+      {chars.map((char, index) => {
+        return <CharDisplay char={char} addToFavs={addToFavs}/>
+      })}
+      </div>
+
+      <div>
+        <h1>Favorites</h1>
+        {favorites.map((char, index) => {
+        return <CharDisplay char={char} />
+      })}
+      </div>
     </div>
   );
 }
--- src/CharDisplay.js
+++ src/CharDisplay.js
@@ -1,9 +1,11 @@
-const CharDisplay = ({char}) => {
+const CharDisplay = ({char, addToFavs}) => {
     // console.log(char)
     return (
         <div>
             <p>{char.name}</p>
             <p>{char.house}</p>
+
+            <button onClick={() => addToFavs(char)}>Add to favorites</button>
         </div>
     )
 }
